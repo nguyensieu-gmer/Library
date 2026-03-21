5d3f4506458de2a3c571fe239359c9b44ab96cad
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-03-21 17:15:39 +0700

    Make book object

--- script.js
+++ script.js
@@ -3,7 +3,62 @@ const dialogadd = document.getElementById("add_dialog");
 const books = document.querySelector(".books");
 const form = dialogadd.querySelector("form");
 
+const myLibrary = [];
 
+
+// book
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.switchStatus = function(){
+    this.read = !(this.read);
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks(){
+    for (let book of myLibrary){
+        let div = document.createElement("div");
+        div.classList.add("book_style");
+
+        let h1Title = document.createElement("h1");
+        h1Title.textContent = book.title;
+        div.appendChild(h1Title);
+
+        let h2Author = document.createElement("h2");
+        h2Author.textContent = book.author;
+        div.appendChild(h2Author);
+
+        let h2Pages = document.createElement("h2");
+        h2Pages.textContent = book.pages + '';
+        div.append(h2Pages);
+
+        let statusBtn = document.createElement("button");
+        if (!book.read){
+            statusBtn.classList.add("not_read");
+            statusBtn.textContent = "Not read";
+        } else {
+            statusBtn.textContent = "Read";
+        }
+        div.appendChild(statusBtn);
+
+        let removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        div.appendChild(removeBtn);
+
+        books.appendChild(div);
+    }
+}
+
+// dialog
 btnaddBook.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -35,8 +90,15 @@ form.addEventListener("submit", (e) => {
     let pages = document.getElementById("pages").value;
     let read =  document.getElementById("read").checked;
 
-    console.log(title, author, pages, read);
+    addBookToLibrary(title, author, pages, read);
+
+    books.innerHTML = "";
+    displayBooks();
 
     dialogadd.close();
 })
 
+// craft
+// addBookToLibrary("a", "b", 12, false)
+// console.log(myLibrary)
+// displayBooks();
